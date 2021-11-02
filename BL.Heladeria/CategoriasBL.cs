c024abec6575c6630ff80b78b15c24ee98568624
KSZV640 <85536270+KSZV640@users.noreply.github.com>
2021-11-01 23:08:21 -0600

    Add files via upload

---
+++ BL.Heladeria/CategoriasBL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Heladeria
+{
+    class CategoriasBL: Categoria
+    {
+        internal object categorias;
+
+        public CategoriasBL(): base()
+        {
+
+        }
+        public DbSet<Categoria> categoria{ get; set; }
+    }
+
+    public class Categoria
+    {
+    }
+}
---
+++ BL.Heladeria/Contexto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Heladeria
+{
+    class Contexto: DbContext
+    {
+        public Contexto(): base("VideoJuegos")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Categoria> categorias { get; set; }
+    }
+}
